75222c8ff077181cd864be13757ec65fabab57e9
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-07-08 14:00:09 +0100

    ✨ Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { type CSSProperties,useMemo } from 'react'
+import { VscGithub, VscLiveShare, VscSourceControl } from 'react-icons/vsc'
+import styled from 'styled-components'
+
+import themeDarkPlus from '~/themes/darkplus'
+
+const FooterContainer = styled.div`
+	grid-area: footer;
+	background-color: ${themeDarkPlus.colours.footer.background};
+	display: flex;
+	flex-direction: row;
+	gap: 12px;
+	padding: 0 12px;
+`
+
+interface WrapperProps {
+	cursor?: CSSProperties['cursor'];
+}
+
+const Wrapper = styled.span<WrapperProps>`
+	color: ${themeDarkPlus.colours.footer.text};
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 4px;
+	padding: 0 4px;
+	&:hover {
+		background-color: ${themeDarkPlus.colours.footer.hoverBackground};
+		cursor: ${props => props.cursor};
+	}
+`
+
+const Text = styled.p`
+	height: 20px;
+`
+
+const Footer = () => {
+
+	const router = useRouter()
+
+	const sharePath = useMemo(() => `https://${process.env.VERCEL_URL ?? ''}${router.asPath}`, [router.asPath])
+
+	return (
+		<FooterContainer>
+
+			<Wrapper>
+				<VscSourceControl size={16} />
+				<Text>master</Text>
+			</Wrapper>
+
+			<Wrapper cursor='pointer' onClick={() => void navigator.clipboard.writeText(sharePath)}>
+				<VscLiveShare />
+				Share
+			</Wrapper>
+
+			<Wrapper cursor='pointer'>
+				<Link
+					href='https://github.com/BlakeIsMyWaifu/Polyfilled'
+					target='_blank'
+					rel='noopener noreferrer'
+					style={{ all: 'inherit' }}
+				>
+					<VscGithub />
+					Github
+				</Link>
+			</Wrapper>
+
+		</FooterContainer>
+	)
+}
+
+export default Footer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { type AppProps } from 'next/app'
 import styled from 'styled-components'
 
 import ActivityBar from '~/components/ActivityBar'
+import Footer from '~/components/Footer'
 import Sidebar from '~/components/Sidebar'
 import TabBar from '~/components/TabBar'
 import { GlobalStyle } from '~/styles/GlobalStyle'
@@ -13,8 +14,8 @@ import { trpc } from '~/utils/trpc'
 const AppContainer = styled.div`
 	display: grid;
 	grid-template-columns: 48px 256px auto;
-	grid-template-rows: 36px auto;
-	grid-template-areas: "activity sidebar tabs" "activity sidebar editor";
+	grid-template-rows: 36px auto 24px;
+	grid-template-areas: "activity sidebar tabs" "activity sidebar editor" "footer footer footer";
 	position: absolute;
 	top: 0;
 	left: 0;
@@ -29,6 +30,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 				<ActivityBar />
 				<Sidebar />
 				<TabBar />
+				<Footer />
 				<Component {...pageProps} />
 			</AppContainer>
 
